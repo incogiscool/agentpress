2b198cf1ee26d889ca634ec519dab89878afee23
incogiscool <94598096+incogiscool@users.noreply.github.com>
2025-10-22 20:53:49 -0400

    Add note to param

--- src/lib/database/models.ts
+++ src/lib/database/models.ts
@@ -3,7 +3,10 @@ import { MethodSchema, ParamSchema, ProjectSchema } from "./schemas";
 
 export const MethodModel =
   mongoose.models.Methods || mongoose.model("Methods", MethodSchema);
+
+// Note: The param model might not be needed right now since we are embedding parameters in methods.
 export const ParamModel =
   mongoose.models.Params || mongoose.model("Params", ParamSchema);
+
 export const ProjectModel =
   mongoose.models.Projects || mongoose.model("Projects", ProjectSchema);
